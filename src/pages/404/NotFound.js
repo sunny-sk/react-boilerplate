fa89131f05446f6d38281f06fd8b6325912912c6
sunny-sk <sk956223@gmail.com>
2021-09-24 13:49:36 +0530

    eslint fixed

--- src/pages/404/NotFound.js
+++ src/pages/404/NotFound.js
@@ -1,11 +1,7 @@
+import { Page } from 'components';
 import React from 'react';
-import {Page} from 'components'
 const NotFound = () => {
-  return (
-    <Page>
-      {/* add page content here */}
-    </Page>
-  );
+  return <Page>{/* add page content here */}</Page>;
 };
 
 export default NotFound;
