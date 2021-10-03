f3d897d5591c266e0f83d22bea311d4aafdac223
sunny-sk <sk956223@gmail.com>
2021-10-04 00:40:37 +0530

    image lazy loading example added

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -2,8 +2,14 @@
 import React from 'react';
 // for lazy loading image
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-const Img = (props) => {
-  return <LazyLoadImage {...props} />;
+const Img = ({ src, alt = '', ...props }) => {
+  return <LazyLoadImage src={src} effect="blur" alt={alt} {...props} />;
 };
 
 export default Img;
+
+//  <Img
+//    height={500}
+//    width={500}
+//    src="https://images.pexels.com/photos/1643409/pexels-photo-1643409.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=1050&w=1000"
+//  />;
